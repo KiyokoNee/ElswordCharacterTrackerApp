690c20247fa99a00c2ca67f20ead86af1f427652
KiyokoNee <k.c.gearing@gmail.com>
2025-02-25 16:58:25 -0600

    Minor edits to Footer links and text

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     return (
         <footer className="bg-dark text-center text-light fixed-bottom">
             <div className="text-center p-3">
-                Made by: Katherine Gearing <a className="text-light" href="https://github.com/KiyokoNee">Github</a>
+                Made by: Katherine Gearing | Github: <a className="text-light" href="https://github.com/KiyokoNee" target="_blank">here</a>
             </div>
         </footer>
     )
