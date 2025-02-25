b12b46132423086f5107aa2aa4b246619d079812
KiyokoNee <k.c.gearing@gmail.com>
2025-02-25 15:38:34 -0600

    Played with the dropdown menu, and updated the page name in index.html

--- client/src/components/DropDownNav.tsx
+++ client/src/components/DropDownNav.tsx
@@ -3,13 +3,15 @@ import {Dropdown} from "react-bootstrap";
 export const DropDownNav = () => {
     return (
         <Dropdown>
-            <Dropdown.Toggle variant="dark" id="dropdown">
+            <Dropdown.Toggle variant="dark" id="main-dropdown">
                 Main Functions
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
                 <Dropdown.Item href="/">All Characters</Dropdown.Item>
                 <Dropdown.Item href="/create-character">Create Character</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item href="#">Create Player</Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     )
