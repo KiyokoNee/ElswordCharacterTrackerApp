d34b533f5caaaef213b8147a96ba6b614cb17d25
KiyokoNee <k.c.gearing@gmail.com>
2025-02-27 12:21:39 -0600

    Main display status is functional on the front end

--- client/src/components/DisplayCharacters.tsx
+++ client/src/components/DisplayCharacters.tsx
@@ -6,17 +6,19 @@ interface Props {
 }
 
 export const DisplayCharacters = ({characters, createCharacter}:Props) => {
+    const user = JSON.parse(sessionStorage.getItem("user"))
 
     return (
         <>
             <button type="button" className="btn btn-primary" onClick={() => createCharacter()}>Create Character</button>
-            <table className="table table-striped">
+            <table className="table table-striped my-3">
                 <thead>
                     <tr>
                         <th>Nickname</th>
                         <th>Character</th>
                         <th>Class</th>
                         <th>Class Code</th>
+                        <th>Your Main</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -27,6 +29,7 @@ export const DisplayCharacters = ({characters, createCharacter}:Props) => {
                             <td>{oneCharacter.characterName}</td>
                             <td>{oneCharacter.role}</td>
                             <td>{oneCharacter.roleId}</td>
+                            <td>{user.mainId === oneCharacter.id ? <span>Main &#127775;</span> : ""}</td>
                         </tr>)
                     }
                 </tbody>
